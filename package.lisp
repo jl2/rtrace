664483347f567b2741dfd409e20f253dea3e7974
Jeremiah LaRocco <jeremiah_larocco@fastmail.com>
2018-08-02 23:20:46 -0600

    Lots of changes...

--- package.lisp
+++ package.lisp
@@ -1,7 +1,7 @@
 ;;;; package.lisp
 
 (defpackage #:rtrace
-  (:use #:cl)
+  (:use #:cl #:stl)
   (:export #:create-scene
            #:add-sphere
            #:add-light
@@ -12,4 +12,5 @@
            #:make-light
            #:run-tests
            #:test-trace
+           #:test-stl
            #:rtrace))
--- rtrace.lisp
+++ rtrace.lisp
@@ -3,16 +3,16 @@
 (in-package #:rtrace)
 
 (defstruct point
-  (x 0.0 :type single-float)
-  (y 0.0 :type single-float)
-  (z 0.0 :type single-float))
+  (x 0.0 :type double-float)
+  (y 0.0 :type double-float)
+  (z 0.0 :type double-float))
 
 (defstruct rvector
-  (x 0.0 :type single-float)
-  (y 0.0 :type single-float)
-  (z 0.0 :type single-float))
+  (x 0.0 :type double-float)
+  (y 0.0 :type double-float)
+  (z 0.0 :type double-float))
 
-(declaim (optimize (speed 3) (safety 1) (debug 2)))
+(declaim (optimize (speed 3) (safety 1) (debug 3)))
 
 (defmacro each-point-to-vector (op p1 p2)
   `(make-rvector :x (funcall ,op (point-x ,p1) (point-x ,p2))
@@ -25,7 +25,7 @@
                  :z (funcall ,op (rvector-z ,p1) (rvector-z ,p2))))
 
 (defun vscale (k v)
-  (declare (type single-float k)
+  (declare (type double-float k)
            (type rvector v))
   (make-rvector :x (* k (rvector-x v))
                 :y (* k (rvector-y v))
@@ -85,9 +85,9 @@
   (direction (make-rvector) :type rvector))
 
 (defstruct rgb-color
-  (r 1.0 :type single-float)
-  (g 1.0 :type single-float)
-  (b 1.0 :type single-float))
+  (r 1.0 :type double-float)
+  (g 1.0 :type double-float)
+  (b 1.0 :type double-float))
 
 (defmacro each-color-to-color (op c1 c2)
   `(make-rgb-color :r (funcall ,op (rgb-color-r ,c1) (rgb-color-r ,c2))
@@ -99,7 +99,7 @@
   (each-color-to-color #'+ c1 c2))
 
 (defun cscale (k c)
-  (declare (type single-float k)
+  (declare (type double-float k)
            (type rgb-color c))
   (make-rgb-color :r (* k (rgb-color-r c))
                   :g (* k (rgb-color-g c))
@@ -119,11 +119,15 @@
   (ka (make-rgb-color :r 0.1 :g 0.1 :b 0.1) :type rgb-color)
   (kr (make-rgb-color :r 0.0 :g 0.0 :b 0.0) :type rgb-color)
   (kt (make-rgb-color :r 0.0 :g 0.0 :b 0.0) :type rgb-color)
-  (ior 1.0 :type single-float))
+  (ior 1.0 :type double-float))
 
 (defstruct sphere
-  (radius 1.0 :type single-float)
+  (radius 1.0 :type double-float)
   (location (make-point) :type point)
+  (material (make-material) :type material))
+
+(defstruct rstl
+  (tris (make-array 0) :type simple-vector)
   (material (make-material) :type material))
 
 (defgeneric material (obj)
@@ -132,8 +136,11 @@
 (defmethod material ((obj sphere))
   (sphere-material obj))
 
+(defmethod material ((obj rstl))
+  (rstl-material obj))
+
 (defstruct rintersection
-  (tval 999999999.0 :type single-float)
+  (tval 999999999.0 :type double-float)
   (intersects nil :type boolean)
   (object nil)
   (ray nil)
@@ -154,15 +161,15 @@
          (lenl (vlength (ray-direction r)))
          (under  (- (* ldoc ldoc) (* (* lenl lenl) (- (* lenoc lenoc) (* rad rad))))))
     (declare (type rvector oc)
-             (type single-float rad lenoc ldoc lenl under))
+             (type double-float rad lenoc ldoc lenl under))
     (cond 
           ((= under 0.0)
-           (let* ((tv (the single-float (/ 
-                                         (the single-float (+ (the single-float (- ldoc)) (the single-float (sqrt (the single-float under)))))
-                                         (the single-float (* lenl lenl)))))
+           (let* ((tv (the double-float (/ 
+                                         (the double-float (+ (the double-float (- ldoc)) (the double-float (sqrt (the double-float under)))))
+                                         (the double-float (* lenl lenl)))))
                  (pt (point-on-ray r tv)))
              (declare (type point pt)
-                      (type single-float tv))
+                      (type double-float tv))
              (make-rintersection
               :tval tv
               :intersects t
@@ -175,7 +182,7 @@
                               (- (- ldoc) (sqrt under))) (* lenl lenl)))
                   (pt (point-on-ray r tv)))
              (declare (type point pt)
-                      (type single-float tv))
+                      (type double-float tv))
              (make-rintersection
               :tval tv
               :intersects t
@@ -183,18 +190,61 @@
               :point pt
               :normal (normalize (psub (sphere-location s) pt)))))
           (t
-           (make-rintersection :tval 0.0 :intersects nil
-                               :object s :ray r )))))
+           nil))))
+
+(defmethod equal-tol (a b &optional (tol 0.0001))
+  (< (abs (- a b)) tol))
+
+(defmethod intersects (r (tri stl:triangle))
+  (with-slots (normal pt1 pt2 pt3) tri
+  (let* (
+         (e1 (psub pt2 pt1))
+           (e2 (psub pt3 pt1))
+           (p (cross (ray-direction r) e2))
+           (a (dot e1 p)))
+      (if (equal-tol a 0.0)
+          (return-from intersects nil))
+      (let* ((f (/ 1.0 a))
+             (s (psub (ray-origin r) pt1))
+             (u (* f (dot s p))))
+        (if (or (< u 0.0) (> u 1.0))
+            (return-from intersects nil))
+        (let* ((q (cross s e1))
+               (v (* f (dot (ray-direction r) q))))
+          (when (or (< v 0.0) (> (+ u v) 1.0))
+            (return-from intersects nil))
+          (make-rintersection :tval (* f (dot e2 q))
+                              :intersects t
+                              :object tri
+                              :ray r
+                              :point (point-on-ray r (* f (dot e2 q)))
+                              :normal (cross e2 e1);; (triangle-normal tri)
+                              ))))))
+
+
+(defmethod intersects (r (tris rstl))
+  (let ((closest nil))
+    (loop for tri across (rstl-tris tris)
+         for next = (intersects r tri) then (intersects r tri)
+         do
+         (when next
+           (if (null closest)
+               (setf closest next)
+               (when (< (rintersection-tval next) (rintersection-tval closest))
+                 (setf closest next)))
+           (setf (rintersection-object closest) tris)))
+    closest))
+
 
 (defstruct light
   (location (make-point) :type point)
   (color (make-rgb-color) :type rgb-color))
 
 (defstruct scene
-  (eye-point (make-point :x 10.0f0 :y 10.0f0 :z 10.0f0) :type point)
+  (eye-point (make-point :x 10.0 :y 10.0 :z 10.0) :type point)
   (look-at (make-point) :type point)
-  (up (make-rvector :x 0.0f0 :y 1.0f0 :z 0.0f0))
-  (aspect (/ 4.0 3.0) :type single-float)
+  (up (make-rvector :x 0.0 :y 1.0 :z 0.0))
+  (aspect (/ 4.0 3.0) :type double-float)
   (objects nil :type list)
   (lights nil :type list))
 
@@ -211,22 +261,30 @@
   (declare (type point origin look-at)
            (type rvector up)
            (type (signed-byte 64) x y width height)
-           (type single-float aspect))
+           (type double-float aspect))
   (let* ((pdiff (normalize (psub origin look-at)))
          (right (cross up pdiff))
-         (rc (vscale (* aspect (+ -0.5f0 (/ x 1.0f0 width))) right))
-         (uc (vscale (+ -0.5 (/ y 1.0f0 height)) up))
+         (rc (vscale (* aspect (+ -0.5 (/ x 1.0 width))) right))
+         (uc (vscale (+ -0.5 (/ y 1.0 height)) up))
          (vdir (vadd pdiff (vadd  rc uc))))
     (declare (type rvector pdiff right rc uc vdir))
     (make-ray :origin origin
-              :direction (if (= 0.0f0 (the single-float (vlength vdir)))
+              :direction (if (= 0.0 (the double-float (vlength vdir)))
                              vdir
                              (normalize vdir)))))
 
+
+(defun clamp (val &key (max 1.0) (min 0.0))
+  (declare (type double-float val max min))
+  (cond ((< val min) min)
+        ((> val max) max)
+        (t val)))
+
+
 (defun set-pixel (img x y rgb)
-  (let ((r (truncate (the single-float (* 255.0f0 (rgb-color-r rgb)))))
-        (g (truncate (the single-float (* 255.0f0 (rgb-color-g rgb)))))
-        (b (truncate (the single-float (* 255.0f0 (rgb-color-b rgb))))))
+  (let ((r (truncate (the double-float (* 255.0 (clamp (rgb-color-r rgb))))))
+        (g (truncate (the double-float (* 255.0 (clamp (rgb-color-g rgb))))))
+        (b (truncate (the double-float (* 255.0 (clamp (rgb-color-b rgb)))))))
     (declare (type fixnum r g b)
              (type (simple-array  (unsigned-byte 8) *) img)
              (type fixnum x y))
@@ -234,48 +292,43 @@
     (setf (aref img y x 1) g)
     (setf (aref img y x 2) b)))
 
-(defun clamp (val &key (max 1.0f0) (min 0.0f0))
-  (declare (type single-float val max min))
-  (cond ((< val min) min)
-        ((> val max) max)
-        (t val)))
-
 (defun shade (scn isect)
   (declare (type scene scn)
-           (type rintersection isect))
-  (let ((ry (rintersection-ray isect)))
-    (declare (type ray ry)
-             (ignorable ry))
-    (if (rintersection-intersects isect)
-        (let ((rval (make-rgb-color :r 0.0f0 :g 0.0f0 :b 0.0f0)))
-          (declare (type rgb-color rval))
-          (dolist (lght (scene-lights scn))
-            (declare (type light lght))
-            (let*  ((ldir (psub (light-location lght) (rintersection-point isect)))
-                    ;; (ldist (vlength ldir))
-                    (nldir (normalize ldir))
-                    (ndl (dot nldir (rintersection-normal isect)))
-                    (intensity (clamp ndl)))
-              (declare (type rvector ldir nldir)
-                       (type single-float ndl intensity))
-              (setf rval (cadd rval (cscale intensity (cmul (light-color lght) (material-kd (material (rintersection-object isect)))))))))
-          rval)
-        
-        (make-rgb-color :r 0.0f0
-                        :g 0.0f0
-                        :b 0.0f0))))
+           (type (or null rintersection) isect))
+    (when (null isect)
+      (return-from shade (make-rgb-color :r 0.0
+                                         :g 0.0
+                                         :b 0.0)))
+    (let ((ry (rintersection-ray isect)))
+      (declare (type ray ry)
+               (ignorable ry))
+      (let ((rval (make-rgb-color :r 0.0 :g 0.0 :b 0.0)))
+        (declare (type rgb-color rval))
+        (dolist (lght (scene-lights scn))
+          (declare (type light lght))
+          (let*  ((ldir (psub (light-location lght) (rintersection-point isect)))
+                  ;; (ldist (vlength ldir))
+                  (nldir (normalize ldir))
+                  (ndl (dot nldir (rintersection-normal isect)))
+                  (intensity (clamp ndl)))
+            (declare (type rvector ldir nldir)
+                     (type double-float ndl intensity))
+            (setf rval (cadd rval (cscale intensity (cmul (light-color lght) (material-kd (material (rintersection-object isect)))))))))
+        rval)))
 
 (defun itrace (scn ry)
   (declare (type scene scn)
            (type ray ry))
-  (let ((nearest (make-rintersection :ray ry)))
-    (declare (type rintersection nearest))
-    (dolist (sph (scene-objects scn))
-      (let ((isect (intersects ry sph)))
-        (declare (type rintersection isect))
-        (if (and (rintersection-intersects isect) (< (rintersection-tval isect) (rintersection-tval nearest)))
-            (setf nearest isect))))
-    (the rgb-color (shade scn nearest))))
+  (let ((nearest nil))
+    (declare (type (or null rintersection) nearest))
+    (dolist (obj (scene-objects scn))
+      (let ((isect (intersects ry obj)))
+        (declare (type (or null rintersection) isect))
+        (if (and (null nearest) isect)
+            (setf nearest isect)
+            (if (and isect (< (rintersection-tval isect) (rintersection-tval nearest)))
+                (setf nearest isect)))))
+    (the rgb-color (shade scn nearest) )))
 
 (defun rtrace (scn file-name &key (width 800) (height 600))
   (declare (type scene scn)
@@ -298,11 +351,11 @@
                     (png:encode img output))))
 
 (defun run-tests ()
-  (let ((v1 (make-rvector :x 1.0f0 :y 0.0f0 :z 0.0f0))
-        (v2 (make-rvector :x 0.0f0 :y 1.0f0 :z 0.0f0))
-        (v3 (make-rvector :x 10.0f0 :y 10.0f0 :z 10.0f0))
-        (p1 (make-point :x 1.0f0 :y 1.0f0 :z 1.0f0))
-        (p2 (make-point :x 0.0f0 :y 0.0f0 :z 0.0f0)))
+  (let ((v1 (make-rvector :x 1.0 :y 0.0 :z 0.0))
+        (v2 (make-rvector :x 0.0 :y 1.0 :z 0.0))
+        (v3 (make-rvector :x 10.0 :y 10.0 :z 10.0))
+        (p1 (make-point :x 1.0 :y 1.0 :z 1.0))
+        (p2 (make-point :x 0.0 :y 0.0 :z 0.0)))
     
     (format t "~a . ~a = ~a~%" v1 v2 (dot v1 v2))
     (format t "~a x ~a = ~a~%" v1 v2 (cross v1 v2))
@@ -313,9 +366,20 @@
     ))
 
 (defun test-trace (fname)
-  (let ((scn (make-scene :eye-point (make-point :x 16.0f0 :y 16.0f0 :z 16.0f0))))
+  (let ((scn (make-scene :eye-point (make-point :x 16.0 :y 16.0 :z 16.0))))
     (add-light scn (make-light :location (make-point :x 40.0 :y 40.0 :z 40.0)))
-    (add-object scn (make-sphere :radius 4.0 :location (make-point :x 8.0)))
+    (add-object scn (make-sphere :radius 4.0
+                                 :location (make-point :x 8.0)
+                                 :material (make-material
+                                            :kd (rtrace:make-rgb-color :r 0.0 :g 1.0 :b 0.0))))
     (add-object scn (make-sphere :radius 4.0))
     (rtrace:rtrace scn fname)))
           
+(defun test-stl (fname stl)
+  (let ((scn (make-scene :eye-point (make-point :x 3.5 :y 5.0 :z 4.0))))
+    (add-light scn (make-light :location (make-point :x 80.0 :y 80.0 :z 80.0)))
+    (add-light scn (make-light :location (make-point :x 80.0 :y 80.0 :z -80.0)))
+    (add-light scn (make-light :location (make-point :x -80.0 :y 80.0 :z -80.0)))
+    (add-light scn (make-light :location (make-point :x -80.0 :y 80.0 :z 80.0)))
+    (add-object scn (make-rstl :tris stl))
+    (Rtrace:rtrace scn fname)))
